40f7edab9a53b13ba27b3afb5d4fa3c4dda4fb50
SUPATKN <yacht1400@gmail.com>
2024-01-31 17:18:12 +0700

    can parse if while block

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws SyntaxError {
-        String s = "if(-1) then move up else move down ";
+        String s = "if(1) then {move up} else {move down} ";
         Tokenizer t = new Tokenizer(s);
         ParserTest p = new ParserTest(t);
         p.ParsePlan();
