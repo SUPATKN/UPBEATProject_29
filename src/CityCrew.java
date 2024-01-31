d7e3d608770ca6d1f796ea41df9ec0a6203cc209
TIT4nnnx <ekarach6627@gmail.com>
2024-01-31 14:31:07 +0700

    Add class -Player (Not yet) -CityCrew (Not yet) -Deposit (Success มั้ง)

---
+++ src/CityCrew.java
@@ -0,0 +1,11 @@
+import javax.swing.text.Position;
+
+public class CityCrew {
+    private String namePlayer;
+    private int[] position = new int[2];
+
+//    public int nearby(){
+//        return 100x+y;
+//    }
+
+}
---
+++ src/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    protected String name;
+    private int Budjet;
+    private CityCrew c = new CityCrew();
+
+
+}
