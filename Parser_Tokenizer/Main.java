19db841f0e37499d4adccedd5193a21f89b45e74
SUPATKN <yacht1400@gmail.com>
2024-02-03 14:18:18 +0700

    edit main class

--- Parser_Tokenizer/Main.java
+++ Parser_Tokenizer/Main.java
@@ -4,9 +4,6 @@ public class Main {
         Map map = new Map(5,5);
         Player p1 = new Player("Y",map);
         p1.Plan();
-//        String s ="if(2) then {} else {}";
-//        Tokenizer t = new Tokenizer(s);
-//        ParserGrammar p = new ParserGrammar(t);
-//        p.ParsePlan();
+
     }
 }
